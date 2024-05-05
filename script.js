18cbda99952dd6a82b27c718630e52e2b0efcbd8
tai.nguyen3 <tai.nguyen3@bnksolution.com>
2024-05-05 13:47:44 +0700

    init firebase

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { collection, getFirestore } from "firebase/firestore";
+const initializeApp = require('irebase/app');
+const getAnalytics = require('firebase/analytics');
+const collection = require('firebase/firestore');
+const getFirestore = require('firebase/firestore');
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +18,18 @@ const firebaseConfig = {
   storageBucket: "mindx-blog-25068.appspot.com",
   messagingSenderId: "633855715996",
   appId: "1:633855715996:web:069f5085989235889b1b59",
-  measurementId: "G-1K0QWX9L1P"
+  measurementId: "G-1K0QWX9L1P",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Init services
+const db = getFirestore();
+
+const colRef = collection(db, "users");
+
+getDocs(colRef).then((snap) => {
+  console.log(snap.docs);
+});
